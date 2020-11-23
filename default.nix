eb23d68e022d28c02764bcc465993a0af0b01d5d
Tony Olagbaiye <me@fron.io>
2020-11-23 15:42:40 +0000

    nix package

--- default.nix
+++ default.nix
@@ -1,9 +1,36 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    buildInputs =  with pkgs; [
-      gcc pkg-config gtk3 webkitgtk glib-networking wrapGAppsHook
-      gdb
-    ];
+{ pkgs ? import <nixpkgs> {}
+, src ? ./.
+, version ? "git"
+, ... }:
 
-    GIO_EXTRA_MODULES="${pkgs.glib-networking}/lib/gio/modules:${pkgs.dconf.lib}/lib/gio/modules";
+pkgs.emacsPackages.trivialBuild rec {
+  pname = "emacs-webkit";
+  inherit src version;
+
+  buildPhase = ''
+    make all
+    export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+  '';
+  nativeBuildInputs = with pkgs; [ pkg-config wrapGAppsHook ];
+  gstBuildInputs = with pkgs; with gst_all_1; [
+    gstreamer gst-libav
+    gst-plugins-base
+    gst-plugins-good
+    gst-plugins-bad
+    gst-plugins-ugly
+  ];
+  buildInputs = with pkgs; [
+    webkitgtk
+    glib gtk3
+    glib-networking
+    gsettings-desktop-schemas
+  ] ++ gstBuildInputs;
+
+  GIO_EXTRA_MODULES = "${pkgs.glib-networking}/lib/gio/modules:${pkgs.dconf.lib}/lib/gio/modules";
+  GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.concatMapStringsSep ":" (p: "${p}/lib/gstreamer-1.0") gstBuildInputs;
+
+  postInstall = ''
+    cp *.so *.js *.css $out/share/emacs/site-lisp/
+    mkdir $out/lib && cp *.so $out/lib/
+  '';
 }
---
+++ shell.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      gcc pkg-config gtk3 webkitgtk glib-networking wrapGAppsHook
+      gdb
+    ];
+
+    GIO_EXTRA_MODULES = "${pkgs.glib-networking}/lib/gio/modules:${pkgs.dconf.lib}/lib/gio/modules";
+}
